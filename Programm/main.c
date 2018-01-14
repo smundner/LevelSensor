a83804099655c1202c10c7c360441e93e28ec000
sebastian Mundner <sebastian.mundner@gmail.com>
2018-01-14 22:44:55 +0200

    add program basics

--- Programm/main.c
+++ Programm/main.c
@@ -1,11 +1,20 @@
 #include <avr/io.h>
 #include "usart.h"
+
+#define analogOut(x) PORTB = x;
+
+void TIMER_Init(){
+
+}
+
 int main(){
 	//
 	USART_Init(103);//9600 baud
-	
+	PORTB=0x00;
+	DDRB=0xff;
+
 	while(1){
-		
+
 	}
 	return 1;
 }
---
+++ onewire.h
@@ -0,0 +1,4 @@
+#ifndef __ONEWIRE_H__
+#define __ONEWIRE_H__
+
+#endif __ONEWIRE_H__
