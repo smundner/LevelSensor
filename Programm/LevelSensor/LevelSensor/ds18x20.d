5d47cbc851a1f8517a1f5e7152eec6b3b96f57d4
Sebastian Mundner <sebastian.mundner@gmail.com>
2018-02-28 00:44:23 +0100

    Makefile modifiziert

---
+++ Programm/LevelSensor/LevelSensor/ds18x20.d
@@ -0,0 +1,45 @@
+ds18x20.d ds18x20.o: ds18x20.c /usr/lib/avr/include/util/delay.h \
+ /usr/lib/avr/include/inttypes.h /usr/lib/gcc/avr/4.8.1/include/stdint.h \
+ /usr/lib/avr/include/stdint.h /usr/lib/avr/include/util/delay_basic.h \
+ /usr/lib/avr/include/math.h /usr/lib/avr/include/avr/interrupt.h \
+ /usr/lib/avr/include/avr/io.h /usr/lib/avr/include/avr/sfr_defs.h \
+ /usr/lib/avr/include/avr/iotn2313.h /usr/lib/avr/include/avr/portpins.h \
+ /usr/lib/avr/include/avr/common.h /usr/lib/avr/include/avr/version.h \
+ /usr/lib/avr/include/avr/fuse.h /usr/lib/avr/include/avr/lock.h \
+ /usr/lib/avr/include/util/atomic.h onewire.h ds18x20.h
+
+/usr/lib/avr/include/util/delay.h:
+
+/usr/lib/avr/include/inttypes.h:
+
+/usr/lib/gcc/avr/4.8.1/include/stdint.h:
+
+/usr/lib/avr/include/stdint.h:
+
+/usr/lib/avr/include/util/delay_basic.h:
+
+/usr/lib/avr/include/math.h:
+
+/usr/lib/avr/include/avr/interrupt.h:
+
+/usr/lib/avr/include/avr/io.h:
+
+/usr/lib/avr/include/avr/sfr_defs.h:
+
+/usr/lib/avr/include/avr/iotn2313.h:
+
+/usr/lib/avr/include/avr/portpins.h:
+
+/usr/lib/avr/include/avr/common.h:
+
+/usr/lib/avr/include/avr/version.h:
+
+/usr/lib/avr/include/avr/fuse.h:
+
+/usr/lib/avr/include/avr/lock.h:
+
+/usr/lib/avr/include/util/atomic.h:
+
+onewire.h:
+
+ds18x20.h:
